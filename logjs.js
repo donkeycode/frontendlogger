4ccf6c9f8a5560a9bdca041b61b7197ee34f44db
Nicola RIZZO <nicola.n.rizzo@gmail.com>
2016-05-27 14:04:00 +0200

    Fix bug maxLengthLog

--- logjs.js
+++ logjs.js
@@ -87,10 +87,10 @@
     }
 
     function addEventSummary(e){
-      if (eventsSummary.length > lenghtMaxLog) {
+      if (eventsSummary.length > maxLenghtLog) {
         eventsSummary.pop();
       }
-      
+
       eventsSummary.push(stringifyEvent(e));
     }
 
